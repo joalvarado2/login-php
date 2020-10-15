5cebdfe16de3153f41fbbfd28d1c6bfe14c60f2a
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-10-15 17:18:47 -0500

    alistando estilos

---
+++ views/contenido.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilos/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar sesión</a>
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus facilis et doloremque quam expedita incidunt! Dolorem fugit quos assumenda. Eos quibusdam praesentium facilis nostrum eaque totam nam ullam autem non.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam laborum amet necessitatibus, neque, molestiae cupiditate perspiciatis at suscipit corrupti veritatis repellat eligendi ipsa inventore, iure minima non rem itaque. Perferendis!</p>
+            </article>
+        </div>
+    </div>
+</body>
+
+</html>
